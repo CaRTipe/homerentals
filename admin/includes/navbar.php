8a1b32c2b3e3b7e10a9c9bf4913aa35ad9ea9c66
CaRTipe <blairmbugua868@gmail.com>
2025-07-27 11:15:38 +0300

    updated the admin navbar to show the Tenants link only on the home page

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,3 +1,7 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+$is_auth_page = ($current_page === 'messages');
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm px-4 py-2">
      <div class="container-fluid d-flex justify-content-between align-items-center">
 
@@ -12,21 +16,29 @@
          <!-- Nav links with spacing -->
          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
              <ul class="navbar-nav gap-3">
+                <?php if (!$is_auth_page): ?>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="index.php">Home</a>
                  </li>
+                 <?php if ($current_page === 'index.php'): ?>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="#tenant">Tenants</a>
                  </li>
+                 <?php endif; ?>
+                 <?php endif; ?>
+                 <?php if ($current_page !== 'messages.php'): ?>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="messages.php">Messages</a>
                  </li>
+                 <?php endif; ?>
+                 <?php if(!$is_auth_page): ?>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="#">Last Month</a>
                  </li>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="#">This Month</a>
-                 </li>
+                    </li>
+                 <?php endif; ?>
                  <?php if (isset($_SESSION['login']) && $_SESSION['login'] == TRUE): ?>
                  <li class="nav-item">
                      <a class="nav-link text-white" href="../logout.php" id="logout">Logout</a>
